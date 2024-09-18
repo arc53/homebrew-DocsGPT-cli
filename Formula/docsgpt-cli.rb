acd4b238e1e43ad496e20604510042459a40bfd1
Pavel <pabin@yandex.ru>
2024-09-18 23:14:33 +0300

    Formula v0.1.0

--- Formula/docsgpt-cli.rb
+++ Formula/docsgpt-cli.rb
@@ -1,17 +1,17 @@
 class DocsgptCli < Formula
   desc "A CLI tool for DocsGPT"
   homepage "https://github.com/arc53/DocsGPT-cli"
-  url "file://#{Dir.home}/Desktop/homebrew-tarballs/DocsGPT-cli-v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/arc53/DocsGPT-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "2d1b0bef8ad54e33de8894ef6f5dbb5b73bcc40a93bb95b4b4b61ccb96428627"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "-o", bin/"docsgpt"
+    system "go", "build", *std_go_args, "-o", bin/"docsgpt-cli"
   end
 
   test do
-    assert_match "DocsGPT-cli version", shell_output("#{bin}/docsgpt --version")
+    assert_match "DocsGPT-cli version", shell_output("#{bin}/docsgpt-cli --version")
   end
 end
