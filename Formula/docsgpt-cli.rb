26cf17d3befe955eaa13524b1a64ff63218a0967
Alex <a@tushynski.me>
2026-08-17 13:44:01 +0100

    Update DocsgptCli to version 1.5.1

--- Formula/docsgpt-cli.rb
+++ Formula/docsgpt-cli.rb
@@ -1,8 +1,8 @@
 class DocsgptCli < Formula
   desc "Command-line client for DocsGPT agents"
   homepage "https://github.com/arc53/DocsGPT-cli"
-  url "https://github.com/arc53/DocsGPT-cli/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "fd199618493074df9cc3fb20c173e20820195ddf52d5eb93b884914ebd86f97e"
+  url "https://github.com/arc53/DocsGPT-cli/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "1ef3769db91914fa21ed5e0d95a1590039cc40168009e234f55c16c010aa2ae5"
   license "MIT"
 
   depends_on "go" => :build
